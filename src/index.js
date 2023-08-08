a849373ed87f98d6ae2ce2f1817ace06e1ceaa82
Omnia Eladawy <omnia.osama@intellaworld.com>
2023-08-08 17:14:08 +0300

    add routers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import RouteLayout from "./pages/RouteLayout";
+import Add from "./pages/Add";
+import Edit from "./pages/Edit";
+import Details from "./pages/Details";
+import Index from "./pages/Index";
 
-import App from "./App";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RouteLayout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "add",
+        element: <Add />,
+      },
+
+      {
+        path: ":id/edit",
+        element: <Edit />,
+      },
+      {
+        path: ":id",
+        element: <Details />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
