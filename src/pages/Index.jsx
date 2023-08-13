81cfa8c443a1e2c18a96a9a7ccc313ce2623c554
Omnia Eladawy <omnia.osama@intellaworld.com>
2023-08-13 13:22:19 +0300

    get data from api using createAsyncThunck and store it in state

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import PostList from "../components/PostList";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../state/postSlice";
 
 const Index = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+  console.log("posts", posts);
+
   return <PostList />;
 };
 
--- src/state/postSlice.js
+++ src/state/postSlice.js
@@ -1,12 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = { records: [], loading: false, error: null };
 
+export const fetchPosts = createAsyncThunk(
+  "posts/fetchPosts",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const res = await fetch("http://localhost:5000/posts");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const postSlice = createSlice({
-  name: "post",
+  name: "posts",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    //fetchPosts
+    [fetchPosts.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.records = action.payload;
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    //createPost
+
+    //editPost
+
+    //deletePost
+  },
 });
 
 export default postSlice.reducer;
